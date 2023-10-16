efe431a2dc5266a735b03002ff27040c83d67899
H0neyHunter <usysstr@gmail.com>
2023-10-16 11:01:36 +0300

    Initial commit

---
+++ app/src/main/java/com/usyssoft/retrofit2_rxjava/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.usyssoft.retrofit2_rxjava.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.usyssoft.retrofit2_rxjava.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
