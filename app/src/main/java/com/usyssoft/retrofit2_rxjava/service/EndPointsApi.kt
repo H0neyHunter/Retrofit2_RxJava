9d3537feb5636f66bc150fb3b4fb704dd73c3786
H0neyHunter <usysstr@gmail.com>
2023-10-16 13:44:18 +0300

    EndPoints

--- app/src/main/java/com/usyssoft/retrofit2_rxjava/service/EndPointsApi.kt
+++ app/src/main/java/com/usyssoft/retrofit2_rxjava/service/EndPointsApi.kt
@@ -5,6 +5,7 @@ import io.reactivex.Single
 import retrofit2.http.GET
 
 interface EndPointsApi {
+    //Endpoints
     @GET("H0neyHunter/Retrofit2_RxJava/master/sample.json")
     fun getCar() : Single<List<CarList>>
 }
